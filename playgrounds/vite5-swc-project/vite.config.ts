7b61f4a73373098899da2ce6909d105e3911af00
JW <jiangweixian1994@gmail.com>
2024-07-06 07:04:06 +0800

    :fire: remove vite@4 and add vite5 with swc

--- playgrounds/vite5-swc-project/vite.config.ts
+++ playgrounds/vite5-swc-project/vite.config.ts
@@ -1,15 +1,11 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { barrel } from 'vite-plugin-barrel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['@mui/material', '@mui/icons-material'],
-    force: true,
-  },
   plugins: [
     react(),
     process.env.ENABLE_BARREL && barrel({ packages: ['@mui/material', '@mui/icons-material'] }),
-  ].filter(Boolean),
+  ],
 })
