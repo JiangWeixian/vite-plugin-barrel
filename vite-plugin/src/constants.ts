459a73ef4789b7562772c78c2b156096690818b1
JW <jiangweixian1994@gmail.com>
2024-11-23 21:08:11 +0800

    :bug: barrel skip non-script files (#16)
    Closes #15

--- vite-plugin/src/constants.ts
+++ vite-plugin/src/constants.ts
@@ -1,4 +1,4 @@
-export const SCRIPT_RE = /\.([cm])?[jt]sx?/
+export const SCRIPT_RE = /\.([cm])?[jt]sx?$/
 export const NODE_MODULES_RE = /node_modules/
 export const POST_FIX_RE = /[?#].*$/s
 
--- vite-plugin/test/utils.test.ts
+++ vite-plugin/test/utils.test.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 
 import { describe, expect, it } from 'vitest'
 
+import { SCRIPT_RE } from '../src/constants'
 import { resolver } from '../src/utils'
 
 describe('resolver', () => {
@@ -10,5 +11,9 @@ describe('resolver', () => {
     const playground = path.join(root, 'playgrounds/vite4-project')
     const result = await resolver('@mui/material', playground)
     expect(result).toBeDefined()
+  })
+  it('is_script', () => {
+    expect(SCRIPT_RE.test('package.json')).toBe(false)
+    expect(SCRIPT_RE.test('package.js')).toBe(true)
   })
 })
